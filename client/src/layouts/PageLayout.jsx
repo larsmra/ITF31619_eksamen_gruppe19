9f591c3081f297b0e1e971d6fba6b81c4ed64a9a
Amalieyr <amalie.y.ressler@hiof.no>
2020-12-01 22:14:10 +0100

    Add import Footer

--- client/src/layouts/PageLayout.jsx
+++ client/src/layouts/PageLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Title from '../components/Title';
+import Footer from '../components/Footer';
 
 const PageLayout = ({ children }) => (
   <>
