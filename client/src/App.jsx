7830ffc179689e0e4995fb0d0d49067b47414a37
larsmra <lars.m.randem@hiof.no>
2020-12-01 15:08:18 +0100

    Changed some file names

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Routes from './routes/Router';
+import Routes from './routes/Routes';
 
 const App = () => <Routes />;
 
---
+++ client/src/routes/Routes.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRoute as Router, Switch, Route } from 'react-router-dom';
+import PageLayout from '../layouts/PageLayout';
+
+import Home from '../pages/Home';
+import Offices from '../pages/Offices';
+import Plumbers from '../pages/Plumbers';
+
+const Routes = () => (
+  <Router>
+    <PageLayout>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/kontorer">
+          <Offices />
+        </Route>
+        <Route path="/kontorer/:id">
+          <Plumbers />
+        </Route>
+      </Switch>
+    </PageLayout>
+  </Router>
+);
+
+export default Routes;
