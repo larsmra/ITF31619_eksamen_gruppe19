101f15674f92d2acdd66818947ae017763c079b3
larsmra <lars.m.randem@hiof.no>
2020-12-04 14:43:18 +0100

    Commented the source of some code

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -1,3 +1,4 @@
+// Denne koden er hentet fra tidligere forelesningsnotater: https://www.dropbox.com/sh/9nr561t0qn6eioo/AAD4E8xcT_7TaAMeU_QSnbbZa/Kodefiler?dl=0&file_subpath=%2Fhifo-leksjon13-master%2Fserver%2Futils%2FjwtToken.js&preview=leksjon_13.zip&subfolder_nav_tracking=1
 export const sendToken = (user, res) => {
   const token = user.getJwtToken();
 
