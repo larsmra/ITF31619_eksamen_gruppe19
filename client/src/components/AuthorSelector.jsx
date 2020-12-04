de5d00c355392d9ddcc332631e8ff40a46971679
Amalieyr <amalie.y.ressler@hiof.no>
2020-12-04 16:35:55 +0100

    Filled AuthorSelector with authors

--- client/src/components/AuthorSelector.jsx
+++ client/src/components/AuthorSelector.jsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const AuthorSelector = () => <select />;
+const AuthorSelector = () => {
+    const [authors] = useState([
+        {name: 'Lars Larsen', value: 1},
+        {name: 'Gunn Gundersen', value: 2},
+        {name: 'Simen Simensen', value: 3}
+    ]);
+
+    return (
+        <select id='author_select' onChange={() => {}}>
+            {authors.map(author => (
+                <option key={author.value} value={author.value}>
+                    {author.name}
+                </option>
+            ))}
+
+        </select>
+    );
+};
 
 export default AuthorSelector;
