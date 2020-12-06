c80294ba8fac3670b3a887afe2d4fffb7d0614b8
Amalieyr <amalie.y.ressler@hiof.no>
2020-12-06 12:45:29 +0100

    Add server router for POST category

--- server/routes/article.js
+++ server/routes/article.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get('/:id', articleController.get);
 router.get('/', articleController.list);
-router.create('/', isAuthenticated(), articleController.create);
+router.post('/', isAuthenticated(), articleController.create);
 router.put('/:id', isAuthenticated(), articleController.update);
 router.delete('/:id', isAuthenticated(), articleController.remove);
 
--- server/routes/category.js
+++ server/routes/category.js
@@ -4,5 +4,6 @@ import { categoryController } from '../controllers/index.js';
 const router = express.Router();
 
 router.get('/', categoryController.list);
+router.post('/', isAuthenticated(), categoryController.create);
 
 export default router;
